3339164c4b10c4092f67741b99edaf82cd2403c4
sirkinshu <shreyanshsawarn88@gmail.com>
2020-08-31 12:57:06 +0530

    LeftRotation

--- LeftRotation.java
+++ LeftRotation.java
@@ -1,42 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-public class DynamicArray {
-	List<Integer> list = new ArrayList<Integer>();
-	List<List<Integer>> seqList = new ArrayList<List<Integer>>();
-	int lastAns;
-	public DynamicArray(int N) {
-		for (int i = 0; i < N; i++) {
-			list = new ArrayList<Integer>();
-			seqList.add(list);
-		}
-	}
-	void putValue(int x, int y, int N) {
-		int rowIndex = (x ^ lastAns) % N;
-		seqList.get(rowIndex).add(y);
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int Q = sc.nextInt();
-		DynamicArray da = new DynamicArray(N);
-		for (int i = 0; i < Q; i++) {
-			int queryType = sc.nextInt();
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			if (queryType == 1) {
-				da.putValue(x, y, N);
-			} else {
-				da.getValue(x, y, N);
-			}
-		}
-		sc.close();
-	}
-	private void getValue(int x, int y, int N) {
-		int colIndex = 0;
-		int rowIndex = (x ^ lastAns) % N;
-		colIndex = y % seqList.get(rowIndex).size();
-		lastAns = seqList.get(rowIndex).get(colIndex);
-		System.out.println(lastAns);
-	}
+public class LeftRotation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int rotation = in.nextInt();
+        int arr[] = new int[n];
+        for (int arr_i = 0; arr_i < n; arr_i++) {
+            arr[arr_i] = in.nextInt();
+        }
+        int[] crtArray = new int[n];
+        int cnt = 0;
+        for (int i = rotation; i < arr.length; i++) {
+            crtArray[cnt] = arr[i];
+            cnt++;
+        }
+        int count = arr.length - rotation;
+        for (int i = 0; i < rotation; i++) {
+            crtArray[count] = arr[i];
+            count++;
+        }
+
+        for (int i = 0; i < crtArray.length; i++) {
+            System.out.print(crtArray[i] + " ");
+        }
+
+    }
+
 }
