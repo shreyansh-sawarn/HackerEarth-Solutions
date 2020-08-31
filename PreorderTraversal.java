b0507989916f80aad84e4aac385bf9fe629d79e8
sirkinshu <shreyanshsawarn88@gmail.com>
2020-08-31 20:45:38 +0530

    PreorderTraversal

---
+++ PreorderTraversal.java
@@ -0,0 +1,14 @@
+class PreorderTraversal {
+	class Node {
+		int data;
+		Node left;
+		Node right;
+	}
+	void preOrder(Node root) {
+		if (root == null)
+			return;
+		System.out.print(root.data + " ");
+		preOrder(root.left);
+		preOrder(root.right);
+	}
+}
