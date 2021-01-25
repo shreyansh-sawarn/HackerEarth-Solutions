cc86b3dfd6fa1ce2b4383ddeb22e2e8779dd3c31
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-01-25 23:51:54 +0530

    Day 8: Dictionaries and Maps

---
+++ 30 Days of Code/Day 8: Dictionaries and Maps.py
@@ -0,0 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+d = {}
+
+for i in range(n):
+    x = input().split()
+    d[x[0]] = x[1]
+
+while True:
+    try:
+        NAME = input()
+        if NAME in d:
+            print(NAME, "=", d[NAME], sep="")
+        else: print("Not found")
+    except:
+        break
