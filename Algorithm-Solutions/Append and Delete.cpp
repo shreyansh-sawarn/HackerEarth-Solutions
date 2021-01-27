211a386580078646cca3e6ce5d8b5da137225e8d
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-01-28 02:15:30 +0530

    Append and Delete

---
+++ Algorithm-Solutions/Append and Delete.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the appendAndDelete function below.
+string appendAndDelete(string s, string t, int k) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string t;
+    getline(cin, t);
+
+    int k;
+    cin >> k;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string result = appendAndDelete(s, t, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
