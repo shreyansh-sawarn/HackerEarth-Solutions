0dd87c8cb93f0309295850a5325f568a81d14eb8
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-01-30 23:46:53 +0530

    Update Lisa's Workbook.cpp

--- Algorithm-Solutions/Lisa's Workbook.cpp
+++ Algorithm-Solutions/Lisa's Workbook.cpp
@@ -1,75 +1,20 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
-vector<string> split_string(string);
-
-// Complete the workbook function below.
-int workbook(int n, int k, vector<int> arr) {
-
-
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string nk_temp;
-    getline(cin, nk_temp);
-
-    vector<string> nk = split_string(nk_temp);
-
-    int n = stoi(nk[0]);
-
-    int k = stoi(nk[1]);
-
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
-
-    vector<string> arr_temp = split_string(arr_temp_temp);
-
-    vector<int> arr(n);
-
-    for (int i = 0; i < n; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
-        arr[i] = arr_item;
+int main() {
+  int n, k, t, page = 1, ans = 0;
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++) {
+    cin >> t;
+    for (int j = 1; j <= t; j++) {
+      int page_no = (j - 1) / k + page;
+      if (j == page_no) {
+        // cout << i << " " << page_no << endl;
+        ans++;
+      }
     }
-
-    int result = workbook(n, k, arr);
-
-    fout << result << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
+    page += (t - 1) / k + 1;
+  }
+  cout << ans << endl;
+  return 0;
 }
