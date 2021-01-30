002c2c7dd59a25598de64ff6977e3c454d20ed3a
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-01-30 23:19:28 +0530

    Modified Kaprekar Numbers

---
+++ Algorithm-Solutions/Modified Kaprekar Numbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the kaprekarNumbers function below.
+void kaprekarNumbers(int p, int q) {
+
+
+}
+
+int main()
+{
+    int p;
+    cin >> p;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    kaprekarNumbers(p, q);
+
+    return 0;
+}
