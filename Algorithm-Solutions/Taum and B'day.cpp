6d8e661fa0aac329ebd45ec1aa93933bf58adce4
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-01-31 00:04:17 +0530

    Taum and B'day

--- Algorithm-Solutions/Taum and B'day.cpp
+++ Algorithm-Solutions/Taum and B'day.cpp
@@ -1,102 +1,19 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-/*
- * Complete the 'taumBday' function below.
- *
- * The function is expected to return a LONG_INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER b
- *  2. INTEGER w
- *  3. INTEGER bc
- *  4. INTEGER wc
- *  5. INTEGER z
- */
-
-long taumBday(int b, int w, int bc, int wc, int z) {
-
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string t_temp;
-    getline(cin, t_temp);
-
-    int t = stoi(ltrim(rtrim(t_temp)));
-
-    for (int t_itr = 0; t_itr < t; t_itr++) {
-        string first_multiple_input_temp;
-        getline(cin, first_multiple_input_temp);
-
-        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
-
-        int b = stoi(first_multiple_input[0]);
-
-        int w = stoi(first_multiple_input[1]);
-
-        string second_multiple_input_temp;
-        getline(cin, second_multiple_input_temp);
-
-        vector<string> second_multiple_input = split(rtrim(second_multiple_input_temp));
-
-        int bc = stoi(second_multiple_input[0]);
-
-        int wc = stoi(second_multiple_input[1]);
-
-        int z = stoi(second_multiple_input[2]);
-
-        long result = taumBday(b, w, bc, wc, z);
-
-        fout << result << "\n";
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    long long b, w, x, y, z;
+    cin >> b >> w >> x >> y >> z;
+    if (x + z < y) {
+      cout << b * x + w * (x + z) << endl;
+    } else if (y + z < x) {
+      cout << b * (y + z) + w * y << endl;
+    } else {
+      cout << b * x + w * y << endl;
     }
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
+  }
+  return 0;
 }
