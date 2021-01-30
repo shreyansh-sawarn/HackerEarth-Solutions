8f2818d7910f6bcb42040cd818a1e3cd8b097803
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-01-31 00:04:32 +0530

    Update The Time in Words.cpp

--- Algorithm-Solutions/The Time in Words.cpp
+++ Algorithm-Solutions/The Time in Words.cpp
@@ -1,30 +1,36 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
-// Complete the timeInWords function below.
-string timeInWords(int h, int m) {
-
-
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    int h;
-    cin >> h;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    int m;
-    cin >> m;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    string result = timeInWords(h, m);
-
-    fout << result << "\n";
-
-    fout.close();
-
-    return 0;
+string s[21] = {"",        "one",     "two",       "three",    "four",
+                "five",    "six",     "seven",     "eight",    "nine",
+                "ten",     "eleven",  "twelve",    "thirteen", "fourteen",
+                "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
+                "twenty"};
+int main() {
+  int h, m;
+  cin >> h >> m;
+  if (m == 0) {
+    cout << s[h] << " o' clock" << endl;
+  } else if (m < 15) {
+    cout << s[m] << " minute" << (m == 1 ? "" : "s") << " past " << s[h]
+         << endl;
+  } else if (m == 15) {
+    cout << "quarter past " << s[h] << endl;
+  } else if (m <= 20) {
+    cout << s[m] << " minutes past " << s[h] << endl;
+  } else if (m < 30) {
+    cout << "twenty " << s[m - 20] << " minutes past " << s[h] << endl;
+  } else if (m == 30) {
+    cout << "half past " << s[h] << endl;
+  } else if (m < 40) {
+    cout << "twenty " << s[40 - m] << " minutes to " << s[h + 1] << endl;
+  } else if (m < 45) {
+    cout << s[60 - m] << " minutes to " << s[h + 1] << endl;
+  } else if (m == 45) {
+    cout << "quarter to " << s[h + 1] << endl;
+  } else {
+    cout << s[60 - m] << " minute" << (m == 59 ? "" : "s") << " to " << s[h + 1]
+         << endl;
+  }
+  return 0;
 }
