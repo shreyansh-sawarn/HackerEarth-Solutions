8e976b7636f1da58f48a578c8196c1aee091f9e8
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-01-31 00:09:21 +0530

    Modified Kaprekar Numbers

--- Algorithm-Solutions/Modified Kaprekar Numbers.cpp
+++ Algorithm-Solutions/Modified Kaprekar Numbers.cpp
@@ -1,24 +1,39 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
-// Complete the kaprekarNumbers function below.
-void kaprekarNumbers(int p, int q) {
-
-
+bool valid(long long num) {
+  long long tmp = num;
+  int d = 0;
+  while (tmp) {
+    tmp /= 10;
+    d++;
+  }
+  tmp = num * num;
+  string str = to_string(tmp);
+  long long r = 0, l = 0;
+  for (int i = str.size() - d; i < str.size(); i++) {
+    r = r * 10 + str[i] - '0';
+  }
+  for (int i = 0; i < str.size() - d; i++) {
+    l = l * 10 + str[i] - '0';
+  }
+  return l + r == num;
 }
 
-int main()
-{
-    int p;
-    cin >> p;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    int q;
-    cin >> q;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    kaprekarNumbers(p, q);
-
-    return 0;
+int main() {
+  long long p, q;
+  cin >> p >> q;
+  int cnt = 0;
+  for (int i = p; i <= q; i++) {
+    if (valid(i)) {
+      cout << i << " ";
+      cnt++;
+    }
+  }
+  if (cnt == 0) {
+    cout << "INVALID RANGE" << endl;
+  } else {
+    cout << endl;
+  }
+  return 0;
 }
