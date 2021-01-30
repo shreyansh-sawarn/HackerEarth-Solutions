a14385130e9c71adc9527b8c4dc468837149c6f5
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-01-31 00:04:14 +0530

    ACM ICPC Team

--- Algorithm-Solutions/ACM ICPC Team.cpp
+++ Algorithm-Solutions/ACM ICPC Team.cpp
@@ -1,79 +1,38 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
+vector<vector<bool> > v;
 
-vector<string> split_string(string);
-
-// Complete the acmTeam function below.
-vector<int> acmTeam(vector<string> topic) {
-
-
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string nm_temp;
-    getline(cin, nm_temp);
-
-    vector<string> nm = split_string(nm_temp);
-
-    int n = stoi(nm[0]);
-
-    int m = stoi(nm[1]);
-
-    vector<string> topic(n);
-
-    for (int i = 0; i < n; i++) {
-        string topic_item;
-        getline(cin, topic_item);
-
-        topic[i] = topic_item;
+int main() {
+  int n, m;
+  cin >> n >> m;
+  v.resize(n);
+  for (int i = 0; i < n; i++) {
+    v[i].resize(m);
+    string str;
+    cin >> str;
+    for (int j = 0; j < m; j++) {
+      v[i][j] = str[j] - '0';
     }
-
-    vector<int> result = acmTeam(topic);
-
-    for (int i = 0; i < result.size(); i++) {
-        fout << result[i];
-
-        if (i != result.size() - 1) {
-            fout << "\n";
+  }
+  int maxTopic = 0, maxCnt = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      int cntTopic = 0;
+      for (int k = 0; k < m; k++) {
+        if (v[i][k] || v[j][k]) {
+          cntTopic++;
         }
+      }
+      if (cntTopic > maxTopic) {
+        maxTopic = cntTopic;
+        maxCnt = 1;
+      } else if (cntTopic == maxTopic) {
+        maxCnt++;
+      }
     }
-
-    fout << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
+  }
+  cout << maxTopic << endl << maxCnt << endl;
+  return 0;
 }
