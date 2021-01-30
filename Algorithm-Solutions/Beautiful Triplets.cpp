1b231d8575a54ae6440c5869753393954a1bfc59
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-01-31 00:04:21 +0530

    Beautiful Triplets

--- Algorithm-Solutions/Beautiful Triplets.cpp
+++ Algorithm-Solutions/Beautiful Triplets.cpp
@@ -1,75 +1,25 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
 using namespace std;
 
-vector<string> split_string(string);
+vector<int> v;
+bool h[21001];
 
-// Complete the beautifulTriplets function below.
-int beautifulTriplets(int d, vector<int> arr) {
-
-
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string nd_temp;
-    getline(cin, nd_temp);
-
-    vector<string> nd = split_string(nd_temp);
-
-    int n = stoi(nd[0]);
-
-    int d = stoi(nd[1]);
-
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
-
-    vector<string> arr_temp = split_string(arr_temp_temp);
-
-    vector<int> arr(n);
-
-    for (int i = 0; i < n; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
-        arr[i] = arr_item;
+int main() {
+  int n, d;
+  cin >> n >> d;
+  for (int i = 0; i < n; i++) {
+    int num;
+    cin >> num;
+    v.push_back(num);
+    h[num] = true;
+  }
+  int cnt = 0;
+  for (int i = 0; i < n; i++) {
+    if (h[v[i] + d] && h[v[i] + d + d]) {
+      cnt++;
     }
-
-    int result = beautifulTriplets(d, arr);
-
-    fout << result << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
+  }
+  cout << cnt << endl;
+  return 0;
 }
