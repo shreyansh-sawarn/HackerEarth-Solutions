9b27d6bd00b340d457e003c16acc08709e94c8fc
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-01-31 22:42:06 +0530

    Intro to Tutorial Challenges

---
+++ Algorithms Solutions/Intro to Tutorial Challenges.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int ar[1000];
+
+int main()
+{
+    int v, n, ans;
+    cin >> v >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ar[i];
+        if (v == ar[i])
+            ans = i;
+    }
+    cout << ans << endl;
+}
