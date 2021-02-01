50cb6cae4ea2396623f45f1aeffbda1f920910df
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-02-01 23:55:23 +0530

    Update Staircase.cpp

--- Algorithms Solutions/Staircase.cpp
+++ Algorithms Solutions/Staircase.cpp
@@ -1,20 +1,13 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
-// Complete the staircase function below.
-void staircase(int n) {
-
-
-}
-
-int main()
-{
-    int n;
-    cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    staircase(n);
-
-    return 0;
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - i - 1; j++) cout << ' ';
+    for (int j = 0; j < i + 1; j++) cout << '#';
+    cout << endl;
+  }
+  return 0;
 }
