35017a500da1af3115893e00c99f8aced9838925
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-02-07 22:58:03 +0530

    Caesar Cipher

---
+++ Algorithms/Caesar Cipher.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    string s;
+    cin >> n >> s >> k;
+    for (int i = 0; i < n; i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z') cout << (char)((s[i] - 'A' + k) % 26 + 'A');
+        else if (s[i] >= 'a' && s[i] <= 'z') cout << (char)((s[i] - 'a' + k) % 26 + 'a');
+        else cout << s[i];
+    }
+    cout << endl;
+    return 0;
+}
