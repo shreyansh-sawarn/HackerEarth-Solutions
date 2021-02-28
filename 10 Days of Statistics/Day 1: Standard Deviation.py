566fbb9d5b93f341773e7dd608bf03d3de59a210
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2021-02-28 22:57:21 +0530

    Day 1: Standard Deviation

---
+++ 10 Days of Statistics/Day 1: Standard Deviation.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N = int(input())
+X = list(map(int, input().strip().split(' ')))
+
+MEAN = sum(X)/N
+sum = 0
+
+for i in range(N):
+    sum += ((X[i]-MEAN)**2)/N
+
+print(round(sum**0.5, 1))
